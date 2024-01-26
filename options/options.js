7d5b80d5cbcd0da86081cf7afe88a0d04be67236
Crowdbotics <team@crowdbotics.com>
2024-01-26 17:41:29 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tenant-landlord-app-45574",
+  url: "https://tenant-landlord-app-45574.botics.co",
+  api: "https://tenant-landlord-app-45574.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
